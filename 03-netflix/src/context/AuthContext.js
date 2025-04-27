bb769a84f1972bb4d9287b47013463af85804752
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-04-27 23:25:08 +0200

    add AuthContext.js

---
+++ 03-netflix/src/context/AuthContext.js
@@ -0,0 +1,17 @@
+import React, { createContext } from 'react'
+
+
+
+
+//* auth(firebase) islemlerini yapacagimiz context alani actik:
+export const YetkiContext = createContext()
+
+const AuthContextProvider = ({children}) => {
+  return (
+    <YetkiContext.Provider value={{}}>
+        {children}
+    </YetkiContext.Provider>
+  )
+}
+
+export default AuthContextProvider
