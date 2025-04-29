71b03a67ae8dd2580f81a4bacbbe306e95f2e418
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-04-29 10:54:19 +0200

    add CardContainer.jsx

--- 03-netflix/src/app/(private)/profile/components/CardContainer.jsx
+++ 03-netflix/src/app/(private)/profile/components/CardContainer.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client";
+
+import React from "react";
 
 const CardContainer = () => {
   return (
-    <div>CardContainer</div>
-  )
-}
+    <div className="flex flex-col md:flex-row items-center justify-center gap-8 mt-10">
+      <div className="w-44 h-44 rounded-md border-2 border-transparent group-hover:border-white overflow-hidden">
+        <div>
+          <img src="./images/default-red.png" alt="profile" />
+        </div>
+        <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white ">
+          {currentUser?.displayName}
+        </div>
+        <div>
+          <img src="./images/default-blue.png" alt="profile" />
+        </div>
+        <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white ">
+          Guest-1
+        </div>
+        <div>
+          <img src="./images/default-green.png" alt="profile" />
+        </div>
+        <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white ">
+          Guest-2
+        </div>
+        <div>
+          <img src="./images/default-slate.png" alt="profile" />
+        </div>
+        <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white ">
+          Guest-3
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CardContainer
+export default CardContainer;
