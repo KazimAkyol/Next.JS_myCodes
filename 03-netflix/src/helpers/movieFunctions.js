25d69a87f746ae3d41d55a52fbbdb15a55769636
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-04-29 12:31:17 +0200

    add movies-page

---
+++ 03-netflix/src/helpers/movieFunctions.js
@@ -0,0 +1,4 @@
+//* movie'lerle ilgili verileri cektigimiz dosya:
+const API_KEY = process.env.TMDB_KEY
+
+//* isetedigimiz her türlü filmi(poplar, upcoming, top_rated, now_playing) fetiren function:
